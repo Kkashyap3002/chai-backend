b518e0a111b74d79e67ec2f9c0e1654734bf8391
Kishan Kashyap <Kashyap3002@gmail.com>
2024-06-25 03:29:02 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,45 @@
+/*
+require('dotenv').config({path: './env'});
+*/
+
+// ----Better version of above code is given below:------------------------------------------------
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+/* --first approach of connecting to a database by making IIFE in index file itself ,used try catch error handling n used async as well
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+
+import express from "express";
+const app = express()
+
+( async() => {
+    try{
+        await mongoose.connect(`${process.env.
+        MONGODB_URI}/${DB_NAME}}`)
+        app.on("error", (error) => {
+            console.log("ERROR:", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on $
+                {process.env.PORT}`);
+        })
+
+    } catch(error){
+        console.error("ERROR:", error)
+        throw err
+    }
+})()
+
+*/
