7edc5c2957b5738ec55c21d628989b36710f8f2c
Kishan Kashyap <Kashyap3002@gmail.com>
2024-07-08 15:53:28 +0530

    add register controller and bug fixes

--- src/models/users.Model.js
+++ src/models/users.Model.js
@@ -55,7 +55,7 @@ const userSchema = new Schema(
 /*** userSchema.pre("save", () => {})   ------this syntax is wrong in pre as arrow fn => in callback does not have ref of this, below one  is correct   ***/
 userSchema.pre("save", async function(next) {
     if(!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password,10)
+    this.password = await bcrypt.hash(this.password,10)
     next()
 })
 
